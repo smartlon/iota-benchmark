571f05d1bdfdf5da0abae14fd5d7f4d6a28f14af
smartlon <782802917@qq.com>
2019-12-03 18:12:21 +0800

    dix bug

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/smartlon/iota-benchmark/docker"
 	"sync"
-	"time"
 )
 
 func main () {
 	var wg1 *sync.WaitGroup
-	wg1 = sync.WaitGroup{}
+	wg1 = &sync.WaitGroup{}
 	wg1.Add(1)
 	go docker.Start(2,wg1)
 	wg1.Wait()
